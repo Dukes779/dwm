410c67443c03d863f74ea61eae156b5418866066
Dukes779 <dunkenvizlo@gmail.com>
2022-06-24 15:24:08 +0200

    goodd

--- config.h
+++ config.h
@@ -31,6 +31,18 @@ static       char *colors[][3]            = {
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
+static const char *tagsel[][2] = {
+	{ "#F5C2E7", "#0B0716" },
+	{ "#DDB6F2", "#0B0716" },
+	{ "#C9CBFF", "#0B0716" },
+	{ "#F28FAD", "#0B0716" },
+	{ "#F2CDCD", "#0B0716" },
+	{ "#ABE9B3", "#0B0716" },
+	{ "#F2CDCD", "#0B0716" },
+	{ "#F28FAD", "#0B0716" },
+	{ "#DDB6F2", "#0B0716" },
+};
+
 static const Rule rules[] = {
 	/* xprop(1):
 	 *	WM_CLASS(STRING) = instance, class

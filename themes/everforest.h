93ef31b73f50098c3846ea44dfe0d302921af0b9
Dukes779 <dunkenvizlo@gmail.com>
2022-09-03 17:57:14 +0200

    everforest

---
+++ themes/everforest.h
@@ -0,0 +1,14 @@
+static   char col_gray1[]   = "#141816";
+static   char col_gray2[]   = "#87c095";
+static   char col_gray3[]   = "#868c80";
+static   char col_gray4[]   = "#cdd6f4";
+static const char cyan[]    = "#87c095";
+
+static   char black[]       = "#e58183";
+static   char gray2[]       = "#d9bb7f"; // unfocused window border
+static   char gray3[]       = "#87c095";
+static   char gray4[]       = "#d39bb6";
+static   char blue[]        = "#83b6af";  // focused window border
+static   char green[]       = "#739b64";
+static   char red[]         = "#d39bb6";
+static   char orange[]      = "#e58183"; 
